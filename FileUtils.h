2d8cb12ffeb747cf6299db7b064109e535850cac
Gkm1724 <gabriel982306@my.yosemite.edu>
2025-11-10 23:53:12 -0800

    Update FileUtils.h

--- FileUtils.h
+++ FileUtils.h
@@ -6,8 +6,8 @@
  * @file FileUtils.h
  * @brief Declares utility functions for handling file operations.
  *
- * @author [Your Name Here]
- * @date [Current Date]
+ * @author [Gabriel Martinez]
+ * @date [11/10/2025]
  * @version 1.0
  */
 
